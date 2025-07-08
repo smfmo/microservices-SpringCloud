7e4b7f9258443c38af4c53245af611e54c9cfd76
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-07-07 22:54:26 -0300

    implementação da comunicação entre o serviço de cartoes e avaliador de c´redito

--- mscreditassessor/src/main/java/io/github/smfmo/mscreditassessor/application/CreditAssessorService.java
+++ mscreditassessor/src/main/java/io/github/smfmo/mscreditassessor/application/CreditAssessorService.java
@@ -1,25 +1,32 @@
 package io.github.smfmo.mscreditassessor.application;
 
+import io.github.smfmo.mscreditassessor.domain.CustomerCard;
 import io.github.smfmo.mscreditassessor.domain.CustomerData;
 import io.github.smfmo.mscreditassessor.domain.CustomerSituation;
+import io.github.smfmo.mscreditassessor.infra.clients.CardsResourceClient;
 import io.github.smfmo.mscreditassessor.infra.clients.CustomerResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CreditAssessorService {
 
     private final CustomerResourceClient customerResourceClient;
+    private final CardsResourceClient cardsResourceClient;
 
     public CustomerSituation getCustomerStatus(String cpf){
         // obter dados do cliente - msclients
         // obter cartões do cliente - mscards
        ResponseEntity<CustomerData> customerDataResponse = customerResourceClient.customerData(cpf);
+       ResponseEntity<List<CustomerCard>> cardsReponse =  cardsResourceClient.getCardsByCustomer(cpf);
        return CustomerSituation
                .builder()
                .customer(customerDataResponse.getBody())
+               .cards(cardsReponse.getBody())
                .build();
     }
 
